6c585aa70c9e8f02fe4bd90c9391ad11c56a55d8
mxdlzg <mxdlzg@163.com>
2018-05-07 22:12:52 +0800

    完成download的server pack的op部分

--- ServerOP/FileRequest.php
+++ ServerOP/FileRequest.php
@@ -59,8 +59,10 @@ class FileRequestRespond extends ServerRespond{
         session_write_close();
 
         //download
-        $nameList = $action->downloadInServer($data);
-        $zipName = $action->pack($nameList);
+        $nameList = FileAction::scanDir(null);  //TODO get true names
+        $pathList = FileAction::scanDir(null);  //TODO get / Path
+        $action->downloadInServer($data,$nameList);
+        $zipName = $action->pack($pathList,$nameList);
         $ossUrl = $action->pushToOSS($zipName);
 
         self::doRespond([Key::TYPE=>ActionType::FILE_DOWNLOAD,Key::DATA=>$ossUrl,Key::MSG=>"打包完毕，链接返回"]);
@@ -75,34 +77,62 @@ class FileRequestRespond extends ServerRespond{
         }
     }
 
-    function downloadInServer($objectBucketPathList){
+    /**
+     * @param $objectBucketPathList
+     * @param $md5Names ,纯粹Name,Name的MD5摘要
+     */
+    function downloadInServer($objectBucketPathList, $md5Names){
         $options = array(
             OssClient::OSS_FILE_DOWNLOAD => null,
         );
-        $names = array();
         try{
-            foreach ($objectBucketPathList as $objectBucketPath){
-                $options[OssClient::OSS_FILE_DOWNLOAD] = "./tmp/".substr($objectBucketPath,strripos($objectBucketPath,"/"));
-                array_push($names,$options[OssClient::OSS_FILE_DOWNLOAD]);
-                $this->ossClient->getObject($this->bucket, $objectBucketPath);
+            for ($i = 0;$i<count($objectBucketPathList);$i++){
+                $options[OssClient::OSS_FILE_DOWNLOAD] = "./tmp/".$md5Names[$i];
+                $this->ossClient->getObject($this->bucket, $objectBucketPathList[$i]);
             }
         } catch(OssException $e) {
             printf(__FUNCTION__ . ": FAILED\n");
             printf($e->getMessage() . "\n");
-            return [];
         }
-        return $names;
     }
 
-    function pack($names){
-        $zipFileName = "";
-        //TODO::
+    /**
+     * 将md5编码的文件按照目标path进行打包
+     * @param $pathList ,将要打包使用的路径和名称
+     * @param $nameList ,摘要名称
+     * @return string
+     */
+    function pack($pathList,$nameList){
+        $rootPath = "./tmp/";
+        $zipFilePath = "./tmp/zip/".md5($pathList[0].$pathList[count($pathList)/2]); //TODO :: 生成唯一的zip编码
 
-        return $zipFileName;
+        $zip=new ZipArchive();
+        if($zip->open($zipFilePath, ZipArchive::OVERWRITE)=== TRUE){
+            $tpName = null;
+            for ($i = 0; $i<count($pathList); $i++){
+                $tpName = $rootPath.$nameList[$i];
+                if (is_file($tpName)){
+                    $zip->addFile($pathList[$i],$tpName);
+                }
+            }
+            $zip->close(); //关闭处理的zip文件
+        }
+        return $zipFilePath;
     }
 
+    /**
+     * Upload zip to OSS
+     * @param $zipName ,Zip Name
+     */
     function pushToOSS($zipName){
-        //TODO::
+        $object = ossZipAimDir.$zipName;
+        try{
+            $result = $this->ossClient->uploadFile($this->bucket,$object,$zipName);
+        }catch (OssException $exception){
+            printf(__FUNCTION__ . ": FAILED\n");
+            printf($exception->getMessage() . "\n");
+            return;
+        }
     }
 
 }
